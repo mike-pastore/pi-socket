afd0e41f9fab876110c4f131fa5da0394a5f4f85
Mike Pastore <mike.t.pastore@gmail.com>
2016-03-06 15:18:48 -0600

    Create reverse function for integer

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ function updateUnicorn (hex) {
   fsData ="import unicornhat as UH\nimport time\nr=" 
       + rgbColor[0] + "\ng=" 
       + rgbColor[1] + "\nb=" 
-      + rgbColor[2] + "\nsleepspeed=0.05\nfor y in range(8):\n\tif (y % 2) == 0:\n\t\tfor x in range(8):\n\t\t\tUH.set_pixel(x,y,r,g,b)\n\t\t\tUH.show()\n\t\t\ttime.sleep(sleepspeed)\n\telse:\n\t\tfor x in range(8[::-1]):\n\t\t\tUH.set_pixel(x,y,r,g,b)\n\t\t\tUH.show()\n\t\t\ttime.sleep(sleepspeed)\ntime.sleep(2)\n";
+      + rgbColor[2] + "\nsleepspeed=0.05\n\ndef reverse(num):\n\treturn int(str(num)[::-1])\n\nfor y in range(8):\n\tif (y % 2) == 0:\n\t\tfor x in range(8):\n\t\t\tUH.set_pixel(x,y,r,g,b)\n\t\t\tUH.show()\n\t\t\ttime.sleep(sleepspeed)\n\telse:\n\t\tfor x in reverse(8):\n\t\t\tUH.set_pixel(x,y,r,g,b)\n\t\t\tUH.show()\n\t\t\ttime.sleep(sleepspeed)\ntime.sleep(2)\n";
 
   fs.writeFile(fsPath, fsData, function(error) {
        if (error) {
